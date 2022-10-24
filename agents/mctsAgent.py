12075170c7738d5d9da0533be56122f98f4af3a3
Jack Oliver <jackoli3@me.com>
2022-10-24 21:39:12 +1100

    patch MCTS algorithm

--- agents/mctsAgent.py
+++ agents/mctsAgent.py
@@ -2,18 +2,25 @@ from mcts_custom import mcts
 from agents.agent import Agent
 
 class playerAgent(Agent):
-    def __init__(self, player, debug=False, render=False):
+    def __init__(self, player, debug=False, render=False, runtime=1):
         super().__init__(player, debug=debug, render=render)
         self.mctsStruct = None
         self.initPlayer = player
         self.debug = debug
         self.render = render
+        self.runtime = runtime
+    
+    def warmup(self, game_state):
+        print("Warming up MCTS agent")
+        assert self.mctsStruct is None
+        self.mctsStruct = mcts(player=self.player, timeLimit = 150000) # 2.5min warmup
 
     def selectAction(self, actions, game_state):
         if self.mctsStruct is None:
             # this is the first time we are called, so we need to initialize the MCTS structure
             assert self.player == self.initPlayer
-            self.mctsStruct = mcts(player=self.player, timeLimit = 1000)
+            self.mctsStruct = mcts(player=self.player, timeLimit = self.runtime*1000)
+        
         
         if self.debug or self.render:
             # action, val = self.mctsStruct.search(initialState=game_state, needDetails=True)
